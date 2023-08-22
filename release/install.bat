e32e59dda71bd1ff04233f5dcfc0591bc7d65b23
Daniel Wiberg <daniel@skillster.se>
2023-08-22 20:37:09 +0200

    Some fixes to the install scripts.

--- release/install.bat
+++ release/install.bat
@@ -5,7 +5,6 @@ rem Change this line to the correct serial port for your computer
 set PORT=COM3
 
 python fnxmgr.zip --port %PORT% --flash-bulk bulk.csv
-python fnxmgr.zip --port %PORT% --boot flash
 
 echo Help Installed
 pause
