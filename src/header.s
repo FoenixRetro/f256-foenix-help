63f02a45f6050978ecd7977549b6a47adb50c870
Daniel Wiberg <daniel@skillster.se>
2023-10-10 15:01:11 +0200

    Add argument and description fields to program header.

--- src/header.s
+++ src/header.s
@@ -3,13 +3,15 @@
 
         EXEC  = __STARTUP_LOAD__
         SLOT  = __STARTUP_LOAD__ >> 13
-        COUNT = __END_LOAD__ >> 13   ; Assumes code starts at $2000.
+        COUNT = __END_LOAD__ >> 13          ; Assumes code starts at $2000.
 
-        .byte   $f2,$56     ; signature
-        .byte   <COUNT      ; block count
-        .byte   <SLOT       ; start slot
-        .word   EXEC        ; exec addr
-        .word   0           ; version
-        .word   0           ; kernel
-        .asciiz "help"      ; name
+        .byte   $f2,$56                     ; signature
+        .byte   <COUNT                      ; block count
+        .byte   <SLOT                       ; start slot
+        .word   EXEC                        ; exec addr
+        .word   0                           ; version
+        .word   0                           ; kernel
+        .asciiz "help"                      ; name
+        .byte   0                           ; arguments
+        .asciiz "SuperBasic help viewer."   ; description
 
